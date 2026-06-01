92e892593044ff02d52be5e8ee57eb720638a373
Alehandro733 <alehandroordnahela@gmail.com>
2026-06-01 13:53:45 +0400

    fixed mfa version

--- mfa_install_env.bat
+++ mfa_install_env.bat
@@ -1,3 +1,3 @@
-CALL "tools\miniforge3\Scripts\activate.bat" 
-conda create --prefix "mfa\mfa_env" -c conda-forge montreal-forced-aligner && conda clean --all -y
+CALL "tools\miniforge3\Scripts\activate.bat"
+conda create --prefix "mfa\mfa_env" -c conda-forge montreal-forced-aligner=3.3.8 -y && conda clean --all -y
 pause
